98509387494414688f13bab54fc779cf00cecc25
28kayak <kaya_ota@msn.com>
2013-03-21 14:53:11 -0700

    add my observation

--- StorageDuration.cpp
+++ StorageDuration.cpp
@@ -20,5 +20,20 @@ int main()
 	{
 		function();
 	}
+
+	cout << "call function() out side of the loop\n";
+	function(); 
 	return 0;
 }
+
+/*
+this program shows the difference between static valiable and non-static(instance)valiable.
+As some web site claims, the big difference is their lifetimes:
+
+*instance valiable is a local valiable belonging to function(), that means the data autoStorage hold is deleted when the program reaches the end of the function.
+change in value:intialize to be 0 ->  show 0 -> add 1(by incrementation) -> reach the end -> release  the value(one cicle is done) -> initialize to be 0 -> show 0 -> add 1 -> reach the end -> release the value(second cicle is done)->.....ten times;  
+
+On the other hand,
+*life time of the static valiable is until the program is done, that means even though program reaches the end of the function, the data the staticStorage hold is NOT released. Thus, increment keeps adding one by one when this function is called.  
+
+*/
