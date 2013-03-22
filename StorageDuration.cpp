8ef5e23fa3094c958141b8fc0a46aab243c17a88
28kayak <kaya_ota@msn.com>
2013-03-21 17:01:09 -0700

    organize lines

--- StorageDuration.cpp
+++ StorageDuration.cpp
@@ -30,10 +30,26 @@ int main()
 this program shows the difference between static valiable and non-static(instance)valiable.
 As some web site claims, the big difference is their lifetimes:
 
-*instance valiable is a local valiable belonging to function(), that means the data autoStorage hold is deleted when the program reaches the end of the function.
-change in value:intialize to be 0 ->  show 0 -> add 1(by incrementation) -> reach the end -> release  the value(one cicle is done) -> initialize to be 0 -> show 0 -> add 1 -> reach the end -> release the value(second cicle is done)->.....ten times;  
+*instance valiable is a local valiable belonging to function(),
+that means the data autoStorage hold is deleted
+when the program reaches the end of the function.
+
+change in value:
+intialize to be 0 
+-> show 0 
+-> add 1(by incrementation) 
+-> reach the end 
+-> release  the value(one cicle is done) 
+-> initialize to be 0 
+-> show 0 
+-> add 1 
+-> reach the end 
+-> release the value(second cicle is done)
+->.....ten times;  
 
 On the other hand,
-*life time of the static valiable is until the program is done, that means even though program reaches the end of the function, the data the staticStorage hold is NOT released. Thus, increment keeps adding one by one when this function is called.  
-
+*life time of the static valiable is until the program is done, 
+that means even though program reaches the end of the function, 
+the data the staticStorage hold is NOT released. 
+Thus, increment keeps adding one by one when this function is called.  
 */
